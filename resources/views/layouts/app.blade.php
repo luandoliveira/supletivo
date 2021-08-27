ea0589c2b66fe88160710792ba5aff00d51924b2
luandoliveira <luanf.d.silva@gmail.com>
2021-08-27 09:14:40 -0400

    add de icon na head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Sistema Supletivo</title>
+    <link rel="sortcut icon" href="img\ss.png" type="image/x-icon" />
+	<title>Sistema Supletivo</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Fonts -->
